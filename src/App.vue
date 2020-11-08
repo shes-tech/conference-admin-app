94105f0bc9742e510acb558a4e3d45cd41535fa0
Italo Lelis <italohdc@gmail.com>
2020-11-08 11:08:12 -0300

    Add Firebase dependencies

--- src/App.vue
+++ src/App.vue
@@ -34,19 +34,16 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({

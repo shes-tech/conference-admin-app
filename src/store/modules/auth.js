fa0bffebbb3903db2f6d51eff5db4fc6378d206a
Italo Lelis <italohdc@gmail.com>
2020-11-08 12:12:28 -0300

    Create login page

---
+++ src/store/modules/auth.js
@@ -0,0 +1,34 @@
+// import Vue from 'vue';
+import firebase from 'firebase/app';
+
+// const db = firebase.firestore();
+// const auth = firebase.auth();
+
+const defaultState = {
+  user: null,
+};
+
+const actions = {
+  login: async ({ commit }, { email, password }) => {
+    const user = await firebase.auth().signInWithEmailAndPassword(email, password);
+    commit('SET_USER', user);
+  },
+};
+
+const getters = {
+  isUserAuthenticated: (state) => Boolean(state.user),
+};
+
+const mutations = {
+  SET_USER: (state, user) => {
+    state.user = user;
+  },
+};
+
+export default {
+  namespaced: true,
+  state: defaultState,
+  getters,
+  actions,
+  mutations,
+};
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <v-form v-model="isFormValid" @submit.prevent="login()">
+      <v-text-field
+        v-model="email"
+        label="Email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Senha"
+        type="password"
+        required
+      ></v-text-field>
+      <v-btn type="submit">LOGIN</v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'Login',
+  data() {
+    return {
+      isFormValid: true,
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    ...mapActions({
+      sendLogin: 'auth/login',
+    }),
+    login() {
+      const { email, password } = this;
+      this.sendLogin({ email, password });
+    },
+  },
 };
 </script>
